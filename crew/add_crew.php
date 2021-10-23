a72cd5469ad65896a058fad11141a54462dc91b8
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2021-10-23 13:47:38 +0800

    adding crew form

---
+++ crew/add_crew.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Seafarer::Add Crew</title>
+</head>
+<body>
+
+<label>First name</label>
+<input type='text' name='fname' placeholder='Enter first name'></input>
+
+<label>Last name</label>
+<input type='text' name='lname' placeholder='Enter last name'></input>
+    
+<label>Address</label>
+<input type='text' name='lname' placeholder='Enter address'></input>
+
+
+<label>Email</label>
+<input type='email' name='lname' placeholder='Enter email'></input>
+
+<label>Contact No</label>
+<input name='number' placeholder='Enter contact no'></input>
+
+<label>Select Rank</label>
+<select name='rank' id=''>
+    <option value='master_captain'>Master/Captain</option>
+    <option value='chief_mate'>Chief Mate</option>
+    <option value='second_mate'>Second Mate</option>
+    <option value='third_mate'>Third Mate</option>
+    <option value='deck_cadet'>Deck Cadet</option>
+    <option value='chief_engineer'>Chief Engineer</option>
+    <option value='second_engineer'>Second Engineer</option>
+    <option value='third_engineer'>Third Engineer</option>
+    <option value='fourth_engineer'>Fourth Engineer</option>
+    <option value='engineer_cadet'>Engineer Cadet</option>
+    <option value='electrician'>Electrician</option>
+    <option value='boatswain'>Boatswain</option>
+    <option value='pump_man'>Pump Man</option>
+    <option value='able_bodied_seaman'>Able-Bodied Seaman</option>
+    <option value='ordinary_seaman'>Ordinary Seaman</option>
+    <option value='fitter'>Fitter</option>
+    <option value='oiler'>Oiler</option>
+    <option value='wiper'>Wiper</option>
+    <option value='chief_cook'>Chief Cook</option>
+    <option value='steward'>Steward</option>
+</select>
+
+
+<label>Select Rank</label>
+<select name='rank' id=''>
+    <option value='master_captain'>Master/Captain</option>
+    <option value='chief_mate'>Chief Mate</option>
+    <option value='second_mate'>Second Mate</option>
+    <option value='third_mate'>Third Mate</option>
+    <option value='deck_cadet'>Deck Cadet</option>
+    <option value='chief_engineer'>Chief Engineer</option>
+    <option value='second_engineer'>Second Engineer</option>
+    <option value='third_engineer'>Third Engineer</option>
+    <option value='fourth_engineer'>Fourth Engineer</option>
+    <option value='engineer_cadet'>Engineer Cadet</option>
+    <option value='electrician'>Electrician</option>
+    <option value='boatswain'>Boatswain</option>
+    <option value='pump_man'>Pump Man</option>
+    <option value='able_bodied_seaman'>Able-Bodied Seaman</option>
+    <option value='ordinary_seaman'>Ordinary Seaman</option>
+    <option value='fitter'>Fitter</option>
+    <option value='oiler'>Oiler</option>
+    <option value='wiper'>Wiper</option>
+    <option value='chief_cook'>Chief Cook</option>
+    <option value='steward'>Steward</option>
+</select>
+
+<select name='ship' id='select_ship'>
+        <!--ajax-->
+</select>
+
+<button type="submit">Save Crew</button>
+
+<!--gawa design para sa terms and responsibilities sa napiling rank,
+yung text baguhin na lang sa js
+-->
+
+</body>
+</html>
