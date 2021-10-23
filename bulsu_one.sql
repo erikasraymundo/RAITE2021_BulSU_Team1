b60eeec5acb5a4641cfbba2f85a62b5a601359e3
erikasraymundo <erika.raymundo.s@bulsu.edu.ph>
2021-10-23 12:11:43 +0800

    Create bulsu_one.sql

---
+++ bulsu_one.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2021 at 06:07 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bulsu_one`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `email` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crew`
+--
+
+CREATE TABLE `crew` (
+  `crew_id` varchar(8) NOT NULL,
+  `fname` varchar(25) NOT NULL,
+  `lname` varchar(25) NOT NULL,
+  `gender` char(1) DEFAULT NULL,
+  `bdate` datetime NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contact_no` varchar(25) NOT NULL,
+  `rank` varchar(50) NOT NULL,
+  `ship_id` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `route`
+--
+
+CREATE TABLE `route` (
+  `route_id` int(11) NOT NULL,
+  `location1` varchar(250) NOT NULL,
+  `geo_point_lat1` double NOT NULL,
+  `geo_point_long1` double NOT NULL,
+  `location2` varchar(250) NOT NULL,
+  `geo_point_lat2` double NOT NULL,
+  `geo_point_long2` double NOT NULL,
+  `distance` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ship`
+--
+
+CREATE TABLE `ship` (
+  `ship_id` varchar(8) NOT NULL,
+  `ship_name` varchar(25) NOT NULL,
+  `speed_class` char(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ship_route`
+--
+
+CREATE TABLE `ship_route` (
+  `ship_route_id` varchar(8) NOT NULL,
+  `eta` double DEFAULT NULL,
+  `ship_id` varchar(8) DEFAULT NULL,
+  `route_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `crew`
+--
+ALTER TABLE `crew`
+  ADD PRIMARY KEY (`crew_id`),
+  ADD KEY `ship_id` (`ship_id`);
+
+--
+-- Indexes for table `route`
+--
+ALTER TABLE `route`
+  ADD PRIMARY KEY (`route_id`);
+
+--
+-- Indexes for table `ship`
+--
+ALTER TABLE `ship`
+  ADD PRIMARY KEY (`ship_id`);
+
+--
+-- Indexes for table `ship_route`
+--
+ALTER TABLE `ship_route`
+  ADD PRIMARY KEY (`ship_route_id`),
+  ADD KEY `ship_id` (`ship_id`),
+  ADD KEY `route_id` (`route_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `route`
+--
+ALTER TABLE `route`
+  MODIFY `route_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `crew`
+--
+ALTER TABLE `crew`
+  ADD CONSTRAINT `crew_ibfk_1` FOREIGN KEY (`ship_id`) REFERENCES `ship` (`ship_id`);
+
+--
+-- Constraints for table `ship_route`
+--
+ALTER TABLE `ship_route`
+  ADD CONSTRAINT `ship_route_ibfk_1` FOREIGN KEY (`ship_id`) REFERENCES `ship` (`ship_id`),
+  ADD CONSTRAINT `ship_route_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `route` (`route_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
